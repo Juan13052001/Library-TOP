f8dee21cbde315ac57d38436f47fbe5ff9260993
Juan <jherrerav1901@alumno.ipn.mx>
2022-04-13 18:14:03 -0500

    Se agrego la funcionalidad de agregar un libro y actualizar las tarjetas de cada libro

--- index.js
+++ index.js
@@ -1,82 +1,118 @@
-let myLibrary = [];
+//* Recuperando el container para colocar las tarjetas de los libros.
+const container = document.querySelector(".container");
 
+//* Iniciando el arreglo de libros
+let myLibrary = [];
+// *Funcion constructora para un nuevo libro
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    // this.info = function () {
-    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${
-    //         this.read ? "read" : "not read"
-    //     }`;
-    // };
 }
-
+//*Prototipo para imprimir la informacion del libro
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
         this.read ? "read" : "not read"
     }`;
 };
 
+//*Boton para agregar un libro
+const agregarLibro = document.getElementById("addBook");
+
+//* Formulario
+const formulario = document.querySelector(".modal-dialog");
+
+//*Abrir el Modal(popup)
+const openModal = document.querySelector("#add");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal-close");
+const isVisible = "is-visible";
+
+//*----------------------------Eventos------------------------------------
+window.addEventListener("keydown", cerrarEsc);
+openModal.addEventListener("click", abrirModal);
+closeModal.addEventListener("click", cerrarModal);
+formulario.addEventListener("submit", addBook);
+
+//*----------------------------Funciones de los eventos-------------------------
+
+//*Funcion para abrir el Modal
+function abrirModal() {
+    formulario.reset();
+    modal.classList.add(isVisible);
+}
+
+//*Funcion para cerrar el Modal
+function cerrarModal(e) {
+    modal.classList.remove(isVisible);
+}
+//*Funcion para cerrar el Modal con la tecla Esc
+
+function cerrarEsc(e) {
+    if (e.key === "Escape") {
+        modal.classList.remove(isVisible);
+    }
+}
+
+//* Funcion que agrega un nuevo libro con la informacion del formulario
+function addBook(e) {
+    e.preventDefault();
+    //* Recuperando los datos del formulario
+    const autor = document.querySelector("#author").value;
+    const title = document.querySelector("#title").value;
+    const pages = document.querySelector("#pages").value;
+    const isRead = document.querySelector("#read").checked;
+
+    //! Agregándolo al array
+    addBookToLibray(new Book(title, autor, pages, isRead));
+    //* Mostrando la lista de libros
+    updateListBooks();
+    //! Removiendo el modal
+    cerrarModal();
+}
+
+//* Funcion que agrega un nuevo libro al arreglo
+
 function addBookToLibray(book) {
     myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const medicoDeCuerpoAlma = new Book(
-    "Medico de Cuerpo y Almas",
-    "Caldwell",
-    450,
-    true
-);
-addBookToLibray(theHobbit);
-addBookToLibray(medicoDeCuerpoAlma);
-addBookToLibray(theHobbit);
-const container = document.querySelector(".container");
-function listBooks(myLibrary) {
-    myLibrary.map((book) => {
-        let libro = document.createElement("div");
-        libro.innerHTML = `<p>Title: ${book.title}</p>`;
-        libro.innerHTML += `<p>Author: ${book.author}</p>`;
-        libro.innerHTML += `<p>Pages: ${book.pages}</p>`;
-        libro.innerHTML += `<p>Read:${book.read}</p>`;
-        libro.classList.add("item-book");
-        container.appendChild(libro);
-    });
+//*Funcion que recorre todo el array, creando una tarjeta de cada libro
+function createCardBooks(book) {
+    const containerCard = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("h3");
+    const pages = document.createElement("h3");
+    const read = document.createElement("h3");
+    const botonDelete = document.createElement("button");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    botonDelete.textContent = "Remove";
+    if (book.read) {
+        read.textContent = "Read";
+    } else {
+        read.textContent = "Not read";
+    }
+
+    containerCard.appendChild(title);
+    containerCard.appendChild(author);
+    containerCard.appendChild(pages);
+    containerCard.appendChild(read);
+    containerCard.appendChild(botonDelete);
+    containerCard.classList.add("item-book");
+    container.appendChild(containerCard);
+}
+function borrarLista() {
+    container.innerHTML = "";
+}
+function updateListBooks() {
+    borrarLista();
+    for (let book of myLibrary) {
+        // borrarLista();
+        createCardBooks(book);
+    }
 }
 
-const agregarLibro = document.getElementById("addBook");
-const botonAgregarLibro = document.querySelector(".modal-dialog");
-const openModal = document.querySelector("#add");
-const modal = document.querySelector(".modal");
-console.log(openModal);
-const closeModal = document.querySelector(".modal-close");
-const isVisible = "is-visible";
-openModal.addEventListener("click", () => {
-    modal.classList.add(isVisible);
-});
-
-closeModal.addEventListener("click", (e) => {
-    modal.classList.remove(isVisible);
-});
-
-window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-        modal.classList.remove(isVisible);
-    }
-});
-let inputAutor = document.querySelector("#author");
-let inputTitle = document.querySelector("#title");
-let inputPages = document.querySelector("#pages");
-
-botonAgregarLibro.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("Enviando informacion");
-
-    let autor = inputAutor.value;
-    let title = inputTitle.value;
-    let paginas = inputPages.value;
-
-    console.log(autor, title, paginas);
-    // const book = new Book(title, author, paginas,)
-});
